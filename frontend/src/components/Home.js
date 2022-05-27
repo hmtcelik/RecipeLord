74583b6f5ebf3897de58113f6abdb3da24b7006b
hmtcelik <abhmtcelik@gmail.com>
2022-05-28 00:29:32 +0300

    show my recipes (filter) testing

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -11,10 +11,13 @@ import RecipeList from "./RecipeList";
 
 const Home = () => {
   const [recipes , setRecipes] = useState([{}]);
+  
+  // testing filter
+  const [newrecipes , setNewRecipes] = useState([]);
+  const [showNewRecipes, setShowNewRecipes] = useState(false);
 
   // check variable for using 'if' in JSX
   const [isPending, setIsPending] = useState(true);
-
 
   // react-spring stuffs
   const springProps = useSpring({ from:{ opacity:0 }, to:{ opacity:1 }, delay: 200, });
@@ -29,21 +32,45 @@ const Home = () => {
         .catch(err => console.log(err));
   }, []);
 
+  const showMyRecipes = () =>{
+    for(let i=0; i<recipes.length; i++){
+      if(Number(localStorage.getItem("user_id")) === (recipes[i].owner)){
+        newrecipes.push(recipes[i]);
+      }
+    }
+    setShowNewRecipes(true);
+  }
+
+  const closeMyRecipes = () =>{
+    setNewRecipes([]);
+    setShowNewRecipes(false);
+  }
+
   return (
     <Container>
       <div className="home"> {/* for container */}
 
         {/* Create Button*/}
         <Row style={{displaymarginTop:"20px"}}>
-            <Link to="/create"><Button color="succes">New Recipe</Button></Link> {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+            <Link to="/create"><Button color="succes">New Recipe</Button></Link>
+        </Row>
+        <br></br>
+        <Row style={{displaymarginTop:"20px"}}>
+            {!showNewRecipes && <Button color="info" onClick={()=>showMyRecipes()}>Show My Recipes</Button>}
+            {showNewRecipes && <Button color="info" onClick={()=>closeMyRecipes()}>Close My Recipes</Button>}
+            {showNewRecipes && <RecipeList recipes={newrecipes} setRecipes={setNewRecipes}/>}
         </Row>
 
         <animated.div style={springProps}>
-          {/* Recipe List */}
-          {isPending && <Row><div className="loader"></div></Row> }
-          {!isPending && <RecipeList recipes={recipes} setRecipes={setRecipes}/>}
-          {!recipes.length && <Row><p>Wow! Such an empty</p></Row> }
-        </animated.div>
+          {!showNewRecipes &&
+          <div>
+            {/* Recipe List */}
+            {isPending && <Row><div className="loader"></div></Row> }
+            {!isPending && <RecipeList recipes={recipes} setRecipes={setRecipes}/>}
+            {!recipes.length && <Row><p>Wow! Such an empty</p></Row> }
+          </div>
+          }
+          </animated.div>
 
       </div>
     </Container>
--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Button, Row } from "reactstrap";
@@ -9,7 +9,7 @@ import IngredientsList from "./IngredientsList";
 const RecipeList = (props) => {
     const recipes = props.recipes;
     const setRecipes = props.setRecipes;
-    
+
     // for finding id on deleting
     const [recipeid, setRecipeId] = useState(0);
 
